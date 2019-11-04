32b693c02fe1cd56532728e08fcdb517dd68fa24
Helge Liebert <helge.liebert@gmail.com>
2019-11-04 18:51:00 -0500

    Update research list.

---
+++ assets/bib/.auctex-auto/wps.el
@@ -0,0 +1,11 @@
+(TeX-add-style-hook
+ "wps"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "Liebert2017phys"
+    "Balestra2017ssa"
+    "Balestra2017peers"
+    "Balestra2017opioid"
+    "Eschenbaum2018reputation"))
+ :bibtex)
+
