6594655739b2b330ac01a987b3edd0400bd511a3
Helge Liebert <helge.liebert@gmail.com>
2019-11-04 19:02:37 -0500

    Build site at 2019-11-05 00:02:37 UTC

--- assets/bib/.auctex-auto/wps.el
+++ assets/bib/.auctex-auto/wps.el
@@ -3,8 +3,8 @@
  (lambda ()
    (LaTeX-add-bibitems
     "Liebert2017phys"
-    "Balestra2017ssa"
     "Balestra2017peers"
+    "Balestra2017ssa"
     "Balestra2017opioid"
     "Eschenbaum2018reputation"))
  :bibtex)
